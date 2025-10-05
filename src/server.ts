9e272e42aa65e38bc466f5e7320cfc2cce9da543
rafi <rafiahmedrabby282@gmail.com>
2025-10-05 19:14:31 +0600

    add

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,24 @@
 import app from "./app"
 import { prisma } from "./config/db"
 import { seedSuperAdmin } from "./modules/admin/seedSuperAdmin"
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
-const connectDb = async () => {
-    try {
-        await prisma.$connect()
-        console.log("******.....Db Connected Successfullyy........ ")
+// const connectDb = async () => {
+//     try {
+        
+//         console.log("******.....Db Connected Successfullyy........ ")
 
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
+//     } catch (error) {
+//         console.log(error)
+//         process.exit(1)
 
-    }
-}
+//     }
+// }
 
 const startServer = async () => {
     try {
-       await connectDb()
-       await seedSuperAdmin()
+        await prisma.$connect()
+        await seedSuperAdmin()
         // app.listen(5000, () => {
         //     console.log("******........Portfolio Server is running...****")
         // })
@@ -28,4 +29,15 @@ const startServer = async () => {
 
 
 }
- startServer()
+
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    
+    try {
+    await startServer() // connect once per cold start
+    app(req, res); // pass request to Express
+  } catch (error) {
+    console.error("Serverless handler error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
