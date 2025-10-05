2a73d1fec440f351ff1dbb892b45c99f734cef37
rafi <rafiahmedrabby282@gmail.com>
2025-10-05 19:52:42 +0600

    structure

--- dist/app/modules/projects/projects.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.projectRouter = void 0;
-const express_1 = require("express");
-const project_controller_1 = require("./project.controller");
-const router = (0, express_1.Router)();
-router.get("/", project_controller_1.ProjectsController.getAllProjects);
-exports.projectRouter = router;
--- dist/app/modules/skills/skills.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.skillRouter = void 0;
-const express_1 = require("express");
-const skills_controller_1 = require("./skills.controller");
-const router = (0, express_1.Router)();
-router.get("/", skills_controller_1.skillsController.getAllSkills);
-exports.skillRouter = router;
